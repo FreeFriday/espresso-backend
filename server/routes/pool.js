b00d9a8d87bd6268dcf934b11b23c4be72fc9833
송명서 <micmic123@snu.ac.kr>
2020-10-31 23:41:35 +0900

    bugfix

--- server/routes/pool.js
+++ server/routes/pool.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/demo', function(req, res) {
-   res.sendFile('../../debug/upload_server2.html');
+   res.sendFile('/home/freefridays/github/espresso-backend/debug/upload_server2.html');
 });
 
 router.get('/author/:id', function(req, res) {
